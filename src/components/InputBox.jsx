00b430e4861a651c26d0629485420668094c08d5
Nitesh Shetye <niteshshetye204@gmail.com>
2024-04-02 09:40:56 +0530

    basic paytm app

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line react/prop-types
+export function InputBox({ label, placeholder, onChange, value }) {
+  return (
+    <div>
+      <div className="text-sm font-medium text-left py-2">{label}</div>
+      <input
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+        className="w-full px-2 py-1 border rounded border-slate-200"
+      />
+    </div>
+  );
+}
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+
+export const Dashboard = () => {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/v1/account/balance", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setBalance(response.data.balance);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={balance} />
+        <Users />
+      </div>
+    </div>
+  );
+};
